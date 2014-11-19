6a39c4989f56c37eb2b0bf2ab64f73b02b865326
Klaus Harrer <klaus.harrer@openforce.com>
2014-11-19 14:07:05 +0100

    RESP-1 added coverage

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -9,6 +9,16 @@ module.exports = function (config) {
         // testing framework to use (jasmine/mocha/qunit/...)
         frameworks: ['jasmine'],
 
+        // coverage reporter generates the coverage
+        reporters: ['progress', 'coverage'],
+
+        preprocessors: {
+          // source files, that you wanna generate coverage for
+          // do not include tests or libraries
+          // (these files will be instrumented by Istanbul)
+          'src/main/webapp/scripts/**/*.js': ['coverage']
+        },
+
         // list of files / patterns to load in the browser
         files: [
             'src/main/webapp/bower_components/modernizr/modernizr.js',
