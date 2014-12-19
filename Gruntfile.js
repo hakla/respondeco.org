dc1983fa3928dfb66556f99725d1e6397c02caac
hklaus <info@hakla.at>
2014-12-20 00:15:03 +0100

    RESP-100 prepared version for testserver

--- Gruntfile.js
+++ Gruntfile.js
@@ -437,8 +437,7 @@ module.exports = function (grunt) {
         'concurrent:test',
         'autoprefixer',
         'connect:test',
-        'karma',
-        'protractor:run'
+        'karma'
     ]);
 
     grunt.registerTask('build', [
