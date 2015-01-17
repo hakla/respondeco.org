5852b4269bc93e903e3cede02239054537037f14
chrisdefa <chrisdefa@gmx.net>
2015-01-17 13:03:29 +0100

    RESP-118 added rating to org page

--- src/main/webapp/scripts/organization/controller_organization.js
+++ src/main/webapp/scripts/organization/controller_organization.js
@@ -15,6 +15,9 @@ respondecoApp.controller('OrganizationController', function($scope, $location, $
     $scope.twitterConnected = false;
     $scope.facebookConnected = false;
 
+    $scope.shownRating = 0;
+    $scope.ratingCount = 0;
+
     $scope.update = function(name) {
         $scope.organization = Organization.get({
             id: name
@@ -46,6 +49,16 @@ respondecoApp.controller('OrganizationController', function($scope, $location, $
             })
         });
     };
+
+    $scope.refreshOrganizationRating = function() {
+        //get new aggregated rating
+        Organization.getAggregatedRating({id: $routeParams.id},
+            function(rating) {
+                $scope.shownRating = rating.rating;
+                $scope.ratingCount = rating.count;
+            });
+    };
+    $scope.refreshOrganizationRating();
 
     $scope.delete = function(id) {
         id = id || $scope.organization.id;
@@ -82,7 +95,7 @@ respondecoApp.controller('OrganizationController', function($scope, $location, $
 
     $scope.redirectToNewProject = function() {
         $location.path('projects/edit/new');
-    }
+    };
 
     if ($routeParams.id !== undefined) {
         $scope.update($routeParams.id);
