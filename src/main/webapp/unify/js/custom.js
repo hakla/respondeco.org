f10c85e5cc72b03cc9ad1e8033dd3434da4efd57
hakla <info@hakla.at>
2015-03-16 19:12:48 +0100

    #84 added angular to onepage, added projects

--- src/main/webapp/unify/js/custom.js
+++ src/main/webapp/unify/js/custom.js
@@ -1,1 +1,34 @@
-/* Write here your custom javascript codes */
+'use strict';
+
+/* App Module */
+
+var respondecoApp = angular.module('respondecoApp', ['http-auth-interceptor', 'tmh.dynamicLocale',
+    'ngResource', 'ngRoute', 'ngCookies', 'pascalprecht.translate', 'truncate']);
+
+respondecoApp
+    .config(function ($routeProvider, $httpProvider, $translateProvider, tmhDynamicLocaleProvider) {
+      $routeProvider.otherwise({
+          controller: 'MainController'
+      });
+    });
+
+respondecoApp.factory('Project', function($resource, $http) {
+    return $resource('app/rest/projects/:id', {}, {
+      query: {
+        isArray: true,
+        method: 'GET',
+        transformResponse: function(data) {
+          return JSON.parse(data).projects;
+        }
+      }
+    });
+});
+
+
+respondecoApp.controller('MainController', function($scope, $location, $rootScope, Project) {
+  $scope.projects = Project.query(function() {
+    setTimeout(function() {
+      App.cubeportfolio();
+    });
+  });
+});
