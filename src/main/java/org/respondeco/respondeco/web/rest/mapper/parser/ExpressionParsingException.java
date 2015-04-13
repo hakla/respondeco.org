35840024470c6e8fb678bb8c37213f883761b6e9
Clemens Puehringer <cl.puehringer@gmail.com>
2015-04-13 21:24:28 +0200

    #85 ExpresionParsingException is now a RuntimeException

--- src/main/java/org/respondeco/respondeco/web/rest/mapper/parser/ExpressionParsingException.java
+++ src/main/java/org/respondeco/respondeco/web/rest/mapper/parser/ExpressionParsingException.java
@@ -3,7 +3,7 @@ package org.respondeco.respondeco.web.rest.mapper.parser;
 /**
  * Created by clemens on 24/03/15.
  */
-public class ExpressionParsingException extends Exception {
+public class ExpressionParsingException extends RuntimeException {
 
     public ExpressionParsingException(String message, Throwable e) {
         super(message, e);
