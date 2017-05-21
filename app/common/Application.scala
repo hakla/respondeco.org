f43afcdb347e03c3717bbe38372dd598835b66de
hakla <info@hakla.at>
2017-05-21 11:03:23 +0200

    Added configuration option for devMode

--- app/common/Application.scala
+++ app/common/Application.scala
@@ -3,16 +3,19 @@ package common
 import javax.inject.Inject
 
 import play.Environment
+import play.api.Configuration
 import play.api.mvc._
 
-class Application @Inject()(environment: Environment) extends Controller {
+class Application @Inject()(environment: Environment, configuration: Configuration) extends Controller {
+
+    val runInDev: Boolean = configuration.getBoolean("mode.dev").getOrElse(false)
 
     def index = Action {
         Ok(views.html.index())
     }
 
     def app = Action {
-        if (environment.isDev) {
+        if (runInDev) {
             Ok(views.html.app_dev())
         } else {
             Ok(views.html.app())
@@ -20,7 +23,7 @@ class Application @Inject()(environment: Environment) extends Controller {
     }
 
     def admin = Action {
-        if (environment.isDev) {
+        if (runInDev) {
             Ok(views.html.admin_dev())
         } else {
             Ok(views.html.admin())
